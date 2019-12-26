a0d34b6db9d8d583238828df23bd7ea9b771bc5a
Your Name <you@example.com>
2019-12-26 13:14:57 +0530

    terraform automation

--- token.sh
+++ token.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kubeadm join 10.0.1.40:6443 --token 1b9qy6.v2rxl5691r30qg3m     --discovery-token-ca-cert-hash sha256:4fcaeab6f7d492ef80227fc464c69a53b278f839ea6d2bfcea2ddf29a869ef4d 
+sudo kubeadm join 10.0.1.245:6443 --token skdmpg.k4cbofi5nq4p93lk     --discovery-token-ca-cert-hash sha256:af102d9f42dd098cff0446020e664749b7ada0ec8dc2dade84b380da4acf8446 
